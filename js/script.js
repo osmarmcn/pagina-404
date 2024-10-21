45f618da1244fb0df121a4623f071392fbdef645
Osmar Mendes <99884631+osmarmcn@users.noreply.github.com>
2024-10-21 12:49:50 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,13 +5,25 @@ let posX = 10, posY = 10;
 
 // Criar paredes do labirinto
 const paredes = [
-    {x: 50, y: 0, width: 10, height: 250},
-    {x: 50, y: 250, width: 200, height: 10},
-    {x: 100, y: 50, width: 150, height: 10},
-    {x: 100, y: 50, width: 10, height: 150},
-    {x: 150, y: 100, width: 100, height: 10},
-    {x: 200, y: 100, width: 10, height: 100},
-    {x: 250, y: 0, width: 10, height: 200}
+    {x: 50, y: 0, width: 10, height: 60},
+    {x: 0, y: 90, width: 70, height: 10},
+    {x: 100, y: 60, width: 150, height: 10},
+    {x: 100, y: 20, width: 10, height: 180},
+    {x: 30, y: 140, width: 70, height: 10},
+    {x: 200, y: 0, width: 10, height: 30},
+    {x: 250, y: 40, width: 10, height: 150},
+    {x: 30, y: 190, width: 10, height: 50},
+    {x: 0, y: 240, width: 150, height: 10},
+    {x: 150, y: 110, width: 10, height: 170},
+    {x: 190, y: 110, width: 10, height: 170},
+    {x: 200, y: 270, width: 40, height: 10},
+    {x: 220, y: 260, width: 10, height: 40},
+    {x: 230, y: 220, width: 70, height: 10},
+    {x: 280, y: 140, width: 20, height: 10},
+    {x: 260, y: 90, width: 20, height: 10},
+    {x: 220, y: 90, width: 10, height: 90},
+    {x: 200, y: 170, width: 30, height: 10},
+   
 ];
 
 paredes.forEach(p => {
